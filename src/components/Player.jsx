cf3d2380bdda74e1d66e55b666bd2a989516c311
Hazem Hashem <hazem_hashem@outlook.com>
2024-05-08 11:34:59 +0300

    editable player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,20 +4,20 @@ import { useState } from "react";
 
 function Player ({ name , symbol }) {
     const [isEditing , setIsEditing] = useState(false);
-    const [pName , setPname] = useState(name)
+    const [playerName , setPlayerName] = useState(name)
     const handleEditButton = () => {
-        setIsEditing(!isEditing);
+        setIsEditing((prevState) => !prevState);
     }
-    const handleSaveButton = () => {
-        setPname()
+    const handleChange = (e) => {
+        setPlayerName(e.target.value)
     }
     return (
         <li>
             <span className="player">
-                {!isEditing ? <span className="player-name" >{name}</span> : <input></input>}
+                {!isEditing ? <span className="player-name" >{playerName}</span> : <input value={playerName} onChange={handleChange}></input>}
               <span className="player-symbol">{symbol}</span>
             </span>
-            {!isEditing ? <button onClick={handleEditButton}>Edit</button> : <button onClick={handleEditButton}>Save</button> }
+            <button onClick={handleEditButton}>{isEditing ? 'Save' : 'Edit'}</button>
           </li>
     )
 }
